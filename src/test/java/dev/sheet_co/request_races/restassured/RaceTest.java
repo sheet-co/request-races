38e3c6d2c197dda8c6b8b53ba352052c5e10abc8
coder <sergeinerobeev@gmail.com>
2025-04-29 10:15:58 +0200

    added Rest Assured

--- src/test/java/dev/sheet_co/request_races/restassured/RaceTest.java
+++ src/test/java/dev/sheet_co/request_races/restassured/RaceTest.java
@@ -1,6 +1,5 @@
 package dev.sheet_co.request_races.restassured;
 
-import dev.sheet_co.request_races.model.entity.Race;
 import dev.sheet_co.request_races.repository.RaceRepository;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -8,8 +7,6 @@ import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -25,30 +22,7 @@ class RaceTest {
     RestAssured.port = 8080;
   }
 
-//  @BeforeEach
-//  void setupRaceTest() {
-//    String raceJson = """
-//        { "name": "SetupRaceTest",
-//          "color": "************"
-//        }
-//        """;
-//    given()
-//        .when()
-//        .contentType(ContentType.JSON)
-//        .body(raceJson)
-//        .post("/api/race")
-//        .then()
-//        .statusCode(201);
-//
-//  }
-
-  @AfterEach
-  void cleanUp() {
-    raceRepository.deleteAll();
-  }
-
   @Test
-  @Order(1)
   @DisplayName("#1")
   void createRaceTest() {
     String raceJson = """
@@ -67,7 +41,6 @@ class RaceTest {
   }
 
   @Test
-  @Order(2)
   @DisplayName("#2")
   void createRaceTest2() {
     String raceJson = """
@@ -86,43 +59,8 @@ class RaceTest {
   }
 
   @Test
-  @Order(3)
   @DisplayName("#3")
   void getRacesTest() {
-
-//    List<Race> races = given()
-//        .contentType(ContentType.JSON)
-//        .when()
-//        .get("/api/race")
-//        .then().log().body().extract().jsonPath().getList("", Race.class);
-//    races.forEach(System.out::println);
-    String raceJson = """
-        { "name": "Fred from 3",
-          "color": "Black"
-        }
-        """;
-    given()
-        .log().all()
-        .when()
-        .contentType(ContentType.JSON)
-        .body(raceJson)
-        .post("/api/race")
-        .then()
-        .statusCode(201);
-
-    String raceJson2 = """
-        { "name": "Molly from 3",
-          "color": "Black"
-        }
-        """;
-    given()
-        .log().all()
-        .when()
-        .contentType(ContentType.JSON)
-        .body(raceJson2)
-        .post("/api/race")
-        .then()
-        .statusCode(201);
 
     ValidatableResponse response = given()
         .contentType(ContentType.JSON)
